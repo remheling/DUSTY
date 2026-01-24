74fa05ccd101e4c4924be2720883ac94c7cfe5ab
remheling <remheling84@gmail.com>
2026-01-25 02:53:31 +0500

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,15 +1,17 @@
 import asyncio
 from aiogram import Bot, Dispatcher
+from aiogram.client.default import DefaultBotProperties
 
 from bot.config import BOT_TOKEN
 from bot.handlers import router
 
 
 async def main():
-    if not BOT_TOKEN:
-        raise RuntimeError("❌ BOT_TOKEN не найден. Проверь .env")
+    bot = Bot(
+        token=BOT_TOKEN,
+        default=DefaultBotProperties(parse_mode="HTML")
+    )
 
-    bot = Bot(BOT_TOKEN, parse_mode="HTML")
     dp = Dispatcher()
     dp.include_router(router)
 
