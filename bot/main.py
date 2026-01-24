cc81bd9b3cd56dd66aaa6cbb021bc65eb596e099
remheling <remheling84@gmail.com>
2026-01-25 01:55:17 +0500

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,8 +1,8 @@
 import asyncio
 from aiogram import Bot, Dispatcher
 
-from config import BOT_TOKEN
-from handlers import router
+from .config import BOT_TOKEN
+from .handlers import router
 
 
 async def main():
@@ -18,4 +18,5 @@ async def main():
 
 if __name__ == "__main__":
     asyncio.run(main())
+
 
