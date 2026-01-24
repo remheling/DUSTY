e2f523a624cdfdfedbb8c5862508539d3ab91366
remheling <remheling84@gmail.com>
2026-01-25 02:48:25 +0500

    Update checker.py

--- bot/checker.py
+++ bot/checker.py
@@ -1,5 +1,4 @@
 from aiogram import Bot
-from aiogram.exceptions import TelegramBadRequest
 from typing import List
 
 
@@ -9,6 +8,7 @@ async def is_subscribed(bot: Bot, user_id: int, channels: List[str]) -> bool:
             member = await bot.get_chat_member(f"@{channel}", user_id)
             if member.status in ("left", "kicked"):
                 return False
-        except TelegramBadRequest:
+        except Exception:
             return False
     return True
+
