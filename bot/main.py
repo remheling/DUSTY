39aa5a24617a4e9594b2a2572233cfed84f925e3
remheling <remheling84@gmail.com>
2026-01-25 16:56:07 +0500

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,25 +1,32 @@
 import asyncio
+import logging
+
 from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
 
-from bot.config import BOT_TOKEN
-from bot.handlers import router
+from config import BOT_TOKEN
+from handlers import router
 
 
 async def main():
+    logging.basicConfig(level=logging.INFO)
+
     bot = Bot(
         token=BOT_TOKEN,
-        default=DefaultBotProperties(parse_mode="HTML")
+        default=DefaultBotProperties(parse_mode=ParseMode.HTML)
     )
 
     dp = Dispatcher()
     dp.include_router(router)
 
+    await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
 
 if __name__ == "__main__":
     asyncio.run(main())
+
 
 
 
