a84e44337d3a891e3595c691d0821d382b0114df
remheling <remheling84@gmail.com>
2026-01-25 18:39:27 +0500

    Update storage.py

--- bot/storage.py
+++ bot/storage.py
@@ -1,16 +1,14 @@
-# storage.py
-
 class ChannelStorage:
     def __init__(self):
         self._channels: list[str] = []
 
     def add_channel(self, username: str):
-        username = username.lower()
+        username = username.lower().lstrip("@")
         if username not in self._channels:
             self._channels.append(username)
 
     def remove_channel(self, username: str) -> bool:
-        username = username.lower()
+        username = username.lower().lstrip("@")
         if username in self._channels:
             self._channels.remove(username)
             return True
