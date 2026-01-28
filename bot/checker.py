85e6a62fbaf08da38511bf39e0ca72d92e108caa
remheling <remheling84@gmail.com>
2026-01-29 02:15:54 +0500

    Update checker.py

--- bot/checker.py
+++ bot/checker.py
@@ -1,24 +1,15 @@
 from aiogram import Bot
-from aiogram.types import ChatMemberStatus
 from storage import storage
 
-async def is_subscribed(bot: Bot, user_id: int) -> tuple[bool, str | None]:
-    if not storage.has_channels():
-        return True, None
-
+async def is_subscribed(bot: Bot, user_id: int) -> bool:
     for channel in storage.get_channels():
         try:
             member = await bot.get_chat_member(channel, user_id)
-            if member.status not in (
-                ChatMemberStatus.MEMBER,
-                ChatMemberStatus.ADMINISTRATOR,
-                ChatMemberStatus.OWNER
-            ):
-                return False, channel
+            if member.status in ("left", "kicked"):
+                return False
         except:
-            return False, channel
-
-    return True, None
+            return False
+    return True
 
 
 
