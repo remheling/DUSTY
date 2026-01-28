4fc4d1f85a70d090ca47cdab610c90b2790684a8
remheling <remheling84@gmail.com>
2026-01-29 02:15:36 +0500

    Update utils.py

--- bot/utils.py
+++ bot/utils.py
@@ -1,1 +1,9 @@
+def parse_time(value: str) -> int | None:
+    if value.endswith("s"):
+        return int(value[:-1])
+    if value.endswith("m"):
+        return int(value[:-1]) * 60
+    if value.endswith("h"):
+        return int(value[:-1]) * 3600
+    return None
 
