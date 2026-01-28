e6778fad1704312f8e2547000c3177138b721111
remheling <remheling84@gmail.com>
2026-01-29 02:14:12 +0500

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,10 +1,12 @@
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-OWNER_ID = int(os.getenv("OWNER_ID"))
+OWNER_ID = int(os.getenv("OWNER_ID", 0))
+
+if not BOT_TOKEN:
+    raise RuntimeError("BOT_TOKEN not set")
+
+
 
 
 
