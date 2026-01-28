3b100ed1cf67cc19957de9a110d008eb8543e256
remheling <remheling84@gmail.com>
2026-01-29 00:56:07 +0500

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,29 +1,19 @@
 import asyncio
 from aiogram import Bot, Dispatcher
-from aiogram.client.default import DefaultBotProperties
 
 from config import BOT_TOKEN
 from handlers import router
-from checker import checker_router  # если есть
+
 
 async def main():
-    bot = Bot(
-        token=BOT_TOKEN,
-        default=DefaultBotProperties(parse_mode="HTML")
-    )
-
+    bot = Bot(BOT_TOKEN)
     dp = Dispatcher()
-
-    # ⬇️ ПОДКЛЮЧАЕМ РОУТЕРЫ ОДИН РАЗ
     dp.include_router(router)
-    dp.include_router(checker_router)
-
     await dp.start_polling(bot)
 
 
 if __name__ == "__main__":
     asyncio.run(main())
-
 
 
 
