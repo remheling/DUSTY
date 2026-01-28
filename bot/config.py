552d64aaee3415c9d6528fd724fd3ead2e770bc5
remheling <remheling84@gmail.com>
2026-01-29 00:54:39 +0500

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,11 +1,9 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-if not BOT_TOKEN:
-    raise RuntimeError("BOT_TOKEN не задан")
-
-OWNER_ID = int(os.getenv("OWNER_ID", "0"))
-if OWNER_ID == 0:
-    raise RuntimeError("OWNER_ID не задан или неверный")
+OWNER_ID = int(os.getenv("OWNER_ID"))
 
 
