004e01bc6757767f8e885b225cc9fa686ad6f91e
remheling <remheling84@gmail.com>
2026-01-29 00:55:24 +0500

    Update checker.py

--- bot/checker.py
+++ bot/checker.py
@@ -1,19 +1,14 @@
 from aiogram import Bot
-from aiogram.exceptions import TelegramBadRequest
-from storage import storage
+from aiogram.types import ChatMember
 
-
-async def get_not_subscribed(bot: Bot, user_id: int) -> list[str]:
-    not_subscribed = []
-
-    for channel in storage.get_channels():
+async def is_subscribed(bot: Bot, user_id: int, channels: list[str]) -> bool:
+    for channel in channels:
         try:
-            member = await bot.get_chat_member(f"@{channel}", user_id)
-            if member.status not in ("member", "administrator", "creator"):
-                not_subscribed.append(channel)
-        except TelegramBadRequest:
-            not_subscribed.append(channel)
-
-    return not_subscribed
+            member: ChatMember = await bot.get_chat_member(channel, user_id)
+            if member.status in ("left", "kicked"):
+                return False
+        except:
+            return False
+    return True
 
 
