8ff77eed351c51f2a20664acd724b9a7ec663e9e
remheling <remheling84@gmail.com>
2026-01-30 02:57:49 +0500

    Update and rename bot/main.py to bot/routers/main.py

--- bot/routers/main.py
+++ bot/routers/main.py
@@ -1,16 +1,22 @@
 import asyncio
 from aiogram import Bot, Dispatcher
+
 from config import BOT_TOKEN
-from handlers import router
+from routers import admin, checker, start
 
 async def main():
-    bot = Bot(BOT_TOKEN, parse_mode="HTML")
+    bot = Bot(BOT_TOKEN)
     dp = Dispatcher()
-    dp.include_router(router)
+
+    dp.include_router(start.router)
+    dp.include_router(admin.router)
+    dp.include_router(checker.router)
+
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
     asyncio.run(main())
+
 
 
 
